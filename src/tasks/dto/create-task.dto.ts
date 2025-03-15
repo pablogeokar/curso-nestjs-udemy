eec5cac7765ac09b0713aaee3cce50f75bd1df9e
Pablo George <pablogeokar@hotmail.com>
2025-03-15 14:37:53 -0300

    validando os DTOs

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,13 @@
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString({ message: 'O nome precisa ser um texto' })
+  @MinLength(5, { message: 'O nome precisa ter no mínimo 5 caracteres' })
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @MinLength(5)
+  @IsNotEmpty()
   readonly description: string;
 }
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,5 +1,9 @@
-export class UpdateTaskDto {
-  readonly name?: string;
-  readonly description?: string;
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateTaskDto } from './create-task.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
+
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+  @IsOptional()
+  @IsBoolean()
   readonly completed?: boolean;
 }
