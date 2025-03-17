3ccdec4dcc6bab2a2f197c22be283a4c04d2a141
Pablo George <pablogeokar@hotmail.com>
2025-03-17 20:04:51 -0300

    crud usuários completo

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,25 +1,41 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   ParseIntPipe,
+  Patch,
   Post,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
   @Get(':id')
-  findOneUSer(@Param('id', ParseIntPipe) id: number) {
+  findOneUser(@Param('id', ParseIntPipe) id: number) {
     return this.userService.findOne(id);
   }
 
   @Post()
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
+  }
+
+  @Patch(':id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.userService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.delete(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -43,6 +44,51 @@ export class UsersService {
         'Falha ao cadastrar usuário!',
         HttpStatus.BAD_REQUEST,
       );
+    }
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    try {
+      const user = await this.prisma.user.findFirst({
+        where: { id },
+      });
+
+      if (!user) {
+        throw new HttpException('Usuário não existe', HttpStatus.BAD_REQUEST);
+      }
+
+      const updateUser = await this.prisma.user.update({
+        where: { id },
+        data: updateUserDto,
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      });
+
+      return updateUser;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async delete(id: number) {
+    try {
+      const user = await this.prisma.user.findFirst({
+        where: { id },
+      });
+
+      if (!user) {
+        throw new HttpException('Usuário não existe', HttpStatus.BAD_REQUEST);
+      }
+
+      await this.prisma.user.delete({ where: { id } });
+      return {
+        message: 'Usuário excluído com sucesso!',
+      };
+    } catch (error) {
+      console.log(error);
     }
   }
 }
